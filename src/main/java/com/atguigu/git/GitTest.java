7c1885d2054277e2b203b0da61089fef3d9023e5
xw <xw@123.com>
2022-02-17 15:11:40 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author xw
+ * @create 2022-02-17 15:03
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+
+
+    }
+}
