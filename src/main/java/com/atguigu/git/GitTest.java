e0f3a025285231db231883c43cf2adafe727d526
xw <xw@123.com>
2022-02-18 20:51:45 +0800

    pull commit2

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("pull test");
+        System.out.println("pull test222");
 
     }
 }
